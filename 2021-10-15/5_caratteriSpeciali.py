182f5c697b1ad2c80bc75228bf0c94348385163b
Simone Giacomini <simogiaco77@gmail.com>
2021-10-17 16:09:57 +0200

    esercizio 5 concluso

---
+++ 2021-10-15/5_caratteriSpeciali.py
@@ -0,0 +1,30 @@
+"""Creare un programma che chieda all’utente di inserire un numero n e stampi
+una stringa lunga n caratteri dove i caratteri che la compongono saranno i
+caratteri @ e # alternati partendo con un carattere @
+Ad esempio se l’utente dice che la stringa dovrà essere lunga 5 caratteri la
+stringa stampata dovrà essere: @#@#@"""
+
+#costanti
+SALUTO ="""CIAO ~Simone Giacomini
+Questo programma chiede in input una lunghezza, e stampa in modo concatenato "@" e "#" in sequenza"
+"""
+
+def stampaSpeciale(cifra, carattere1="@", carattere2="#"):
+    stringa=""
+    for e in range(0,cifra):
+        if e%2==0 :
+            stringa+=carattere1
+        else:
+            stringa+=carattere2
+    return stringa
+
+print (SALUTO)
+lunghezza="hola"
+while not lunghezza.isnumeric():
+    lunghezza = input("Inserisci la lunghezza della stringa speciale da creare\n-> ")
+    
+    if not lunghezza.isnumeric():
+        print("\n>>>>VALORE INSERITO NON è UN NUMERO INTERO<<<<<\n")
+
+lunghezza=int(lunghezza)
+print("La stringa speciale creata è {}".format(stampaSpeciale(lunghezza)))
